23c985841fc26e28aec55e31ec52b1dd5f2ed51e
Nesibe ŞAHİN <45272038+42Nesibe@users.noreply.github.com>
2022-10-11 17:06:09 +0300

    diziler_2
    diziler 2

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -9,37 +9,51 @@ namespace ConsoleApp2
     internal class Program
     {
         static void Main(string[] args)
-        {
-            string[] renkler=new string[5];
-            string[] hayvanlar = {"kedi", "kopek", "inek", "sinek"};
-            int[] dizi;
-            dizi=new int[5];
+        { 
+            int[] dizi = {15,25,65,75,8,7,88,12};
+            foreach (var item in dizi)
+	        {
+                Console.WriteLine(item);
+            }
+	        
+             Console.WriteLine("\n///////////////\n");
+            
+            Array.Sort(dizi);
+            foreach (var item in dizi)
+	        {
+                Console.WriteLine(item);
+            }
 
-            renkler[0]="siyah";
-            renkler[1]="beyaz";
-            dizi[0]=5;
-            dizi[3]=4;
+            Console.WriteLine("\n///////////////\n");
+            
+            Array.Clear(dizi,2,2);
+             foreach (var item in dizi)
+	         {
+                Console.WriteLine(item);
+             }
 
-            Console.WriteLine(renkler[1]);
-            Console.WriteLine(dizi[3]);
-            Console.WriteLine(hayvanlar[0]);
+             Console.WriteLine("\n///////////////\n");
 
-            Console.WriteLine("\n***************\n");
+            Array.Reverse(dizi);
+             foreach (var item in dizi)
+	         {
+                Console.WriteLine(item);
+             }
 
-            Console.WriteLine("Lütfen dizi elaman sayisini giriniz:");
-            int sayi_uzunluk=int.Parse(Console.ReadLine());
-            int[] sayiDizisi= new int[sayi_uzunluk];
-            int toplam=0;
-            for (int i = 0; i < sayi_uzunluk; i++)
-			{
-                Console.Write("{0}. elamanı giriniz: ",i+1);
-                sayiDizisi[i]=int.Parse(Console.ReadLine());
-                toplam += sayiDizisi[i];
-			}
+             Console.WriteLine("\n///////////////\n");
 
-            Console.WriteLine("Ortalama : "+ toplam/sayi_uzunluk);
+             Console.WriteLine(Array.IndexOf(dizi,65));
 
+              Console.WriteLine("\n///////////////\n");
 
+             Array.Resize<int>(ref dizi,9);
+            dizi[8]=2;
+             foreach (var item in dizi)
+	         {
+                Console.WriteLine(item);
+             }
+
+           
            Console.ReadLine();
         }
     }
