00db2279d0a35465335e0b986ea12cf8312f33b5
Nesibe ŞAHİN <45272038+42Nesibe@users.noreply.github.com>
2022-10-12 21:32:20 +0300

    fonksiyonlar_3
    fonksiyonlar_3

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace ConsoleApp2
 {
@@ -10,42 +12,99 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            string sayi="123";
-            bool sonuc=int.TryParse(sayi,out int outSayi);
-            if(sonuc)
-                Console.WriteLine("İşlem başarılı :"+ outSayi);
-            else
-                Console.WriteLine("işlem başarısız");
+          int result=1;
+            for (int i = 1; i < 5; i++)
+			{
+                result=result*3;
+			}
+            Console.WriteLine(result);
 
-            
-            Metotlar sinif=new Metotlar();
-            sinif.Topla(4,5,out int toplamSonuc);
-            sinif.EkranaYazdir(toplamSonuc);
-            sinif.EkranaYazdir("Nesibe","şahin");
-            sinif.EkranaYazdir("7");
+            Islemler sinif_islem=new Islemler();
+            Console.WriteLine(sinif_islem.Kuvvet(3,4));
+
+            string ifade="Nesibe ŞAHİN";
+
+           
+            bool sonux=ifade.BoslukVarMi();
+            Console.Write(sonux);
+
+            Console.WriteLine(ifade.BuyukHarfeCevir());
+            Console.WriteLine(ifade.KucukHarfeCevir());
+            int[] dizi = {1,4,8,75,3};
+            dizi.KucuktenBuyugeSirala();
+            dizi.EkranaYazdir();
+
+            int sayi=5;
+           Console.WriteLine(ifade.IlkKarakter());
+           Console.WriteLine(sayi.CiftSayiMi());
+
+
 
             Console.ReadLine();
         }
         
     }
 
-    class Metotlar
+    public class Islemler
     {
-       public void Topla(int a,int b , out int toplam)
+        public int Kuvvet(int sayi,int us)
         {
-            toplam=a+b;
-        }
-        public void EkranaYazdir(string veri)
-        {
-            Console.WriteLine(veri);
-        }
-        public void EkranaYazdir(int veri)
-        {
-            Console.WriteLine(veri);
-        }
-        public void EkranaYazdir(string veri1, string veri2)
-        {
-            Console.WriteLine(veri1+ veri2);
+            if(us<2)
+                return sayi;
+            return Kuvvet(sayi,us-1)*sayi;
         }
     }
+
+    public static class Extension
+    {
+        public static bool  BoslukVarMi(this string param)
+        {
+            return param.Contains(" ");
+        }
+
+        
+     
+
+      // public static string ElemaDegistir(this string param)
+       // {
+       //     string[] dizi=param.Split(" ");
+       //     return string.Join(" ",dizi);
+       // }
+
+        public static string BuyukHarfeCevir(this string param)
+        {
+            return param.ToUpper();
+        }
+
+        public static string KucukHarfeCevir(this string param)
+        {
+            return param.ToLower();
+        }
+
+        public static int[] KucuktenBuyugeSirala(this int[] param)
+        {
+            Array.Sort(param);
+            return param;
+        }
+
+        public static void EkranaYazdir(this int[] param)
+        {
+            foreach (var item in param)
+                Console.WriteLine(item);
+        }
+
+        public static bool CiftSayiMi(this int param)
+        {
+            return param%2==0;
+        }
+
+        public static string IlkKarakter(this string param)
+        {
+            return param.Substring(0,1);
+        }
+
+
+    }
+
+    
  }
