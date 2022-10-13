63a67b31209cf8e0ff761dca4efdc3959d29097b
Nesibe ŞAHİN <45272038+42Nesibe@users.noreply.github.com>
2022-10-13 15:59:59 +0300

    string_metot
    string metotlar

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -12,83 +12,41 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-             Console.WriteLine("************* ODEV1 ************ ");
+            string degisken1="Dersimiz CSharp, Hoşgeldiniz";
+            string degisken2="CSharp";
 
-             Console.Write("Kac adet sayi girmek istiyorsunuz: ");
-             int sayi1=int.Parse(Console.ReadLine());
-             int[] dizi1=new int[sayi1];
-             for (int i = 0; i < sayi1; i++)
-	         {
-                Console.Write("sayi giriniz :");
-                dizi1[i]=int.Parse(Console.ReadLine());
-	         }
-             Console.WriteLine("Girilen sayilardan çift sayi olanlar ");
-             foreach (var item in dizi1)
-	         {
-                if(item%2==0)
-                    Console.WriteLine(item);
-	         }
-
-
-
-             Console.WriteLine("************* ODEV2 ************ ");
-
-             Console.Write("ilk sayıyı giriniz: ");
-             int sayi2_1=int.Parse(Console.ReadLine());
-             Console.Write("ikinci sayıyı giriniz: ");
-             int sayi2_2=int.Parse(Console.ReadLine());
-             int[] dizi2=new int[sayi2_1];
-             for (int i = 0; i < sayi2_1; i++)
-	         {
-                Console.Write("sayi giriniz :");
-                dizi2[i]=int.Parse(Console.ReadLine());
-	         }
-             Console.WriteLine("Girilen sayilardan eşit veya tam bölünen sayilar ");
-             foreach (var item in dizi2)
-	         {
-                if(item%sayi2_2==0 || item==sayi2_2)
-                    Console.WriteLine(item);
-	         }
+            Console.WriteLine(degisken1.Length);         // karakter sayısı
+            Console.WriteLine(degisken1.ToUpper());      // buyuk harfe çevir
+            Console.WriteLine(degisken1.ToLower());      // kucuk harfe cevir
+            Console.WriteLine(string.Concat(degisken1, "Merhaba")); // degisken1 ekleme yapma
+            Console.WriteLine(degisken1.CompareTo(degisken2));  //-1,0,,1
+            Console.WriteLine(string.Compare(degisken1, degisken2,true));
+            Console.WriteLine(string.Compare(degisken1, degisken2,false));
+            Console.WriteLine(degisken1.Contains(degisken2));
+            Console.WriteLine(degisken1.EndsWith("Hoşgeldiniz"));
+            Console.WriteLine(degisken1.StartsWith("Merhaba"));
+            Console.WriteLine(degisken1.IndexOf("CS"));
+            Console.WriteLine(degisken1.IndexOf("Nesibe"));
+            Console.WriteLine(degisken1.LastIndexOf("Merhaba"));
+            Console.WriteLine(degisken2.Insert(0,"Merhaba "));
+            Console.WriteLine(degisken1+degisken2.PadLeft(30));
+            Console.WriteLine(degisken1+degisken2.PadLeft(30,'*'));
+            Console.WriteLine(degisken1.PadRight(50)+degisken2);
+            Console.WriteLine(degisken1.PadRight(50,'*')+degisken2);
+            Console.WriteLine(degisken1.Remove(10));
+            Console.WriteLine(degisken1.Remove(5,3));
+            Console.WriteLine(degisken1.Remove(0,1));
+            Console.WriteLine(degisken1.Replace(" ","*"));
+           
+             Console.WriteLine(degisken1.Replace("CSharp","C#"));
+          //  Console.WriteLine(degisken1.Split('')[1]);
+            Console.WriteLine(degisken1.Substring(4));
+            Console.WriteLine(degisken1.Substring(4,6));
 
 
 
-             Console.WriteLine("************* ODEV3 ************ ");
-
-             Console.Write("Kac adet kelime girmek istiyorsunuz: ");
-             int sayi3=int.Parse(Console.ReadLine());
-             string[] dizi3=new string[sayi3];
-             for (int i = 0; i < sayi3; i++)
-	         {
-                Console.Write("kelime giriniz :");
-                dizi3[i]=Console.ReadLine().ToString();
-             }
-             Array.Reverse(dizi3);
-             Console.WriteLine("Girilen kelimlerin sondan başa doğru yazılışı");
-             foreach (var item in dizi3)
-                    Console.WriteLine(item);
-
-
-
-             Console.WriteLine("************* ODEV4 ************ ");
-             Console.Write("Bir cümle yazınız: ");
-             harfSay(Console.ReadLine());
-
-
-             Console.ReadLine();
+            Console.ReadLine();
         }
-        private static void harfSay(string cumle)
-        {
-            if (!string.IsNullOrWhiteSpace(cumle))
-            {
-                int sayac = cumle.Length;
-                string harf = cumle.Substring(0, 1);
-                cumle = cumle.Replace(harf, "");
-                sayac -= cumle.Length;
-                Console.WriteLine($"{harf} -> {sayac} Adet");
-                harfSay(cumle);
-            }
-        }
-        
     }
 
     
