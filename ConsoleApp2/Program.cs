e7951fc5d92f80991b1daed4f6954ee35296e024
Nesibe ŞAHİN <45272038+42Nesibe@users.noreply.github.com>
2022-10-13 21:05:08 +0300

    datetimeAndMath
    Datetime ve Math Kütüphaneleri

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -12,38 +12,66 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            string degisken1="Dersimiz CSharp, Hoşgeldiniz";
-            string degisken2="CSharp";
+             Console.WriteLine("************** DATETİME ***********\n");
+             Console.WriteLine(DateTime.Now);
+             Console.WriteLine(DateTime.Now.Date);
+             Console.WriteLine(DateTime.Now.Day);
+             Console.WriteLine(DateTime.Now.Month);
+             Console.WriteLine(DateTime.Now.Year);
+             Console.WriteLine(DateTime.Now.Hour);
+             Console.WriteLine(DateTime.Now.Minute);
+             Console.WriteLine(DateTime.Now.Second);
 
-            Console.WriteLine(degisken1.Length);         // karakter sayısı
-            Console.WriteLine(degisken1.ToUpper());      // buyuk harfe çevir
-            Console.WriteLine(degisken1.ToLower());      // kucuk harfe cevir
-            Console.WriteLine(string.Concat(degisken1, "Merhaba")); // degisken1 ekleme yapma
-            Console.WriteLine(degisken1.CompareTo(degisken2));  //-1,0,,1
-            Console.WriteLine(string.Compare(degisken1, degisken2,true));
-            Console.WriteLine(string.Compare(degisken1, degisken2,false));
-            Console.WriteLine(degisken1.Contains(degisken2));
-            Console.WriteLine(degisken1.EndsWith("Hoşgeldiniz"));
-            Console.WriteLine(degisken1.StartsWith("Merhaba"));
-            Console.WriteLine(degisken1.IndexOf("CS"));
-            Console.WriteLine(degisken1.IndexOf("Nesibe"));
-            Console.WriteLine(degisken1.LastIndexOf("Merhaba"));
-            Console.WriteLine(degisken2.Insert(0,"Merhaba "));
-            Console.WriteLine(degisken1+degisken2.PadLeft(30));
-            Console.WriteLine(degisken1+degisken2.PadLeft(30,'*'));
-            Console.WriteLine(degisken1.PadRight(50)+degisken2);
-            Console.WriteLine(degisken1.PadRight(50,'*')+degisken2);
-            Console.WriteLine(degisken1.Remove(10));
-            Console.WriteLine(degisken1.Remove(5,3));
-            Console.WriteLine(degisken1.Remove(0,1));
-            Console.WriteLine(degisken1.Replace(" ","*"));
+             Console.WriteLine(DateTime.Now.DayOfWeek);
+             Console.WriteLine(DateTime.Now.DayOfYear);
+
+            
+             Console.WriteLine(DateTime.Now.ToShortDateString());
+             Console.WriteLine(DateTime.Now.ToLongDateString());
+             Console.WriteLine(DateTime.Now.ToLongTimeString());
+             Console.WriteLine(DateTime.Now.ToShortTimeString());
+
+             Console.WriteLine(DateTime.Now.AddDays(2));
+             Console.WriteLine(DateTime.Now.AddHours(3));
+             Console.WriteLine(DateTime.Now.AddSeconds(5));
+             Console.WriteLine(DateTime.Now.AddMonths(5));
+             Console.WriteLine(DateTime.Now.AddYears(2));
+             Console.WriteLine(DateTime.Now.AddMilliseconds(7));
+
+             Console.WriteLine(DateTime.Now.ToString("dd"));
+             Console.WriteLine(DateTime.Now.ToString("ddd"));
+             Console.WriteLine(DateTime.Now.ToString("ddd"));
+
+             Console.WriteLine(DateTime.Now.ToString("MM"));
+             Console.WriteLine(DateTime.Now.ToString("MMM"));
+            Console.WriteLine(DateTime.Now.ToString("MMMM"));
+
+             Console.WriteLine(DateTime.Now.ToString("yy"));
+             Console.WriteLine(DateTime.Now.ToString("yyyy"));
+
+             Console.WriteLine("\n************** MATH ***********\n");
+
+            Console.WriteLine(Math.Abs(-25)); //mutlak
+            Console.WriteLine(Math.Sin(10));
+            Console.WriteLine(Math.Cos(10));
+            Console.WriteLine(Math.Tan(10));
+
+            Console.WriteLine(Math.Ceiling(22.3)); //uste yuvarla
+            Console.WriteLine(Math.Round(22.3)); // en yakına
+            Console.WriteLine(Math.Round(22.7));
+            Console.WriteLine(Math.Floor(22.7)); // alta yuvarla
+
+           Console.WriteLine(Math.Max(2,6));
+            Console.WriteLine(Math.Min(22,85));
+
+            Console.WriteLine(Math.Pow(3,4)); // üslü ifade
+            Console.WriteLine(Math.Sqrt(9));  // karekök
+            Console.WriteLine(Math.Log(9));
+           Console.WriteLine(Math.Exp(3));  // e üzeri 3
+            Console.WriteLine(Math.Log(10));
+
+
            
-             Console.WriteLine(degisken1.Replace("CSharp","C#"));
-          //  Console.WriteLine(degisken1.Split('')[1]);
-            Console.WriteLine(degisken1.Substring(4));
-            Console.WriteLine(degisken1.Substring(4,6));
-
-
 
             Console.ReadLine();
         }
