08f256e96c6766991e8c75ebde3c0c72a6583b5b
Nesibe ŞAHİN <45272038+42Nesibe@users.noreply.github.com>
2022-10-14 15:20:50 +0300

    ArrayList
    ArrayList

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Remoting.Messaging;
@@ -12,106 +13,51 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            List<int> sayiListesi= new List<int>();
-            List<string> renkListesi= new List<string>();
+          ArrayList liste=new ArrayList();  
+           liste.Add("Ayşe");
+           liste.Add(2);
+           liste.Add(true);
+           liste.Add('A');
 
-            sayiListesi.Add(10);
-            sayiListesi.Add(4);
-            sayiListesi.Add(6);
-            sayiListesi.Add(12);
-            sayiListesi.Add(20);
-            sayiListesi.Add(1);
-            sayiListesi.Add(70);
+            Console.WriteLine(liste[1]);
+            foreach (var item in liste)
+                Console.WriteLine(item);
 
-            renkListesi.Add("kırmızı");
-            renkListesi.Add("sarı");
-            renkListesi.Add("mavi");
-            renkListesi.Add("yeşil");
-            renkListesi.Add("siyah");
-            renkListesi.Add("mor");
+            Console.WriteLine("******* Add Range  *********");
+            string[] renkler = {"kırmızı","sarı","yeşil" };
+            liste.AddRange(renkler);
 
-            Console.WriteLine(sayiListesi.Count);
-            Console.WriteLine(renkListesi.Count);
+             foreach (var item in liste)
+                Console.WriteLine(item);
 
-            Console.WriteLine("\n*******   Sayılar   *******\n");
-            foreach (var item in sayiListesi)
-	            Console.WriteLine(item);
+            ArrayList liste2=new ArrayList();
+            List<int> sayilar=new List<int>(){1,5,7,8,2,6};
+            liste2.AddRange(sayilar);
 
-            Console.WriteLine("\n*******   Renkler   ********\n");
-            foreach (var item in renkListesi)
-	            Console.WriteLine(item);
+             Console.WriteLine("****************");
+             foreach (var item in liste2)
+                Console.WriteLine(item);
 
-            sayiListesi.Remove(4);
-            renkListesi.Remove("yeşil");
+              Console.WriteLine("***************");
 
-            sayiListesi.RemoveAt(1);
-            renkListesi.RemoveAt(1);
+             liste2.Sort();
+             foreach (var item in liste2)
+                Console.WriteLine(item);
 
-            Console.WriteLine("\n*******   Sayılar   *******\n");
-            sayiListesi.ForEach(sayi=> Console.WriteLine(sayi));
+             Console.WriteLine("***************");
 
-            Console.WriteLine("\n*******   Renkler   ********\n");
-            renkListesi.ForEach(sayi=> Console.WriteLine(sayi));
+            Console.WriteLine(liste2.BinarySearch(5));
 
-            if(sayiListesi.Contains(12))          //liste içeriside arama
-                Console.WriteLine("\n12 liste içerisnde bulundu... ");
+            liste2.Reverse();
+             foreach (var item in liste2)
+                Console.WriteLine(item);
 
-            Console.WriteLine(renkListesi.BinarySearch("Sarı"));   //Elemanın indexine erişme
-
-            string[] hayvanlar = {"kedi", "kopek", "inek", "tavsan"};
-            List<string> hayvanListesi= new List<string>(hayvanlar);     //diziyi listeye aktarma
-
-            hayvanListesi.Clear();
-
-
-            List<Kullanicilar> kullanicilarListesi= new List<Kullanicilar>();
-            Kullanicilar kullanici1=new Kullanicilar();
-            kullanici1.Isim="Nesibe";
-            kullanici1.Soyisim="ŞAHİN";
-            kullanici1.Yas=23;
-
-            Kullanicilar kullanici2=new Kullanicilar();
-            kullanici2.Isim="Fırat Servan";
-            kullanici2.Soyisim="ÜÇKARDEŞ";
-            kullanici2.Yas=29;
-            kullanicilarListesi.Add(kullanici1);
-            kullanicilarListesi.Add(kullanici2);
-
-            List<Kullanicilar> yeniListe=new List<Kullanicilar>();
-            yeniListe.Add(new Kullanicilar()
-            {
-                Isim="Nesibe",
-                Soyisim="ŞAHİN",
-                Yas=28
-            });
-
-            foreach (var item in kullanicilarListesi)
-	        {
-                Console.WriteLine("Kullanıcı adı: "+item.Isim);
-                Console.WriteLine("Kullanıcı soyadı: "+item.Soyisim);
-                Console.WriteLine("Kullanıcı yaşı: "+item.Yas);
-	        }
-
-
-
-
-
-
-
+             liste2.Clear();
+            
 
             Console.ReadLine();
         }
     }
-    public class Kullanicilar
-    {
-         private string isim ;
-         private string soyisim;
-         private int yas;
-
-        public int Yas { get => yas; set => yas = value; }
-        public string Isim { get => isim; set => isim = value; }
-        public string Soyisim { get => soyisim; set => soyisim = value; }
-
-    }
+   
     
  }
